074068fd910f140016004a4698dd29ce2c12ac85
wwt2008 <wwt2008@gmail.com>
2018-10-06 20:38:12 +0300

    Changed routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/films', 'FilmsController@index');
+Route::get('/home', 'FilmsController@index');
+Route::get('/', 'FilmsController@index');
+
+Route::get('/films/create', 'FilmsController@create')->name('films.create');
+Route::get('/films/edit/{id}', 'FilmsController@edit')->name('films.edit');
+Route::post('/films/store', 'FilmsController@store')->name('films.store');
+Route::post('/films/update', 'FilmsController@update')->name('films.update');
+Route::post('/films/delete', 'FilmsController@delete')->name('films.delete');
+Route::get('/films/{slug}', 'FilmsController@show')->name('films.show');
+Route::get('/films/deletephoto/{id}', 'FilmsController@deletephoto')->name('films.deletephoto');
+
+Route::post('/comments/add', 'CommentsController@create')->name('comments.add');
+
+Route::get('/countries/add', 'FilmsController@addcountries');
+Route::get('/genres/add', 'FilmsController@addgenres');
+
+Auth::routes();
